5f6911d13cc23d4252e5c1290b50b3ada96db703
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2017-09-10 17:51:53 -0400

    Assessment00 + assessment01 merged + rspec

---
+++ master_list/lib/master_list.rb
@@ -0,0 +1,64 @@
+# assessment01
+def caesar_cipher(str, shift)
+
+end
+
+def digital_root
+
+end
+
+def digital_root_step
+
+end
+
+def jumble_sort(str, alphabet = nil)
+
+end
+
+def factors(num)
+
+
+class Array
+  def two_sum
+
+  end
+end
+
+class String
+  def real_words_in_string(dictionary)
+
+  end
+
+end
+
+#RECURSION
+#assessment01
+
+def is_prime?(num)
+end
+
+def primes(num)
+end
+
+def factorials_rec(num)
+end
+
+class String
+  def symmetric_substrings
+  end
+end
+
+class Array
+  def my_inject(accumulator = nil)
+  end
+
+  def dups
+  end
+
+  def merge_sort(&prc)
+  end
+
+  private
+  def self.merge(left, right, &prc)
+  end
+end
